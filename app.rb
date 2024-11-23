cc45a0dd3d45f81c4d4296e607b4fd69a17fc696
T.M. <ace071144@gmail.com>
2024-11-23 23:13:02 +0900

    Rubyでダウンロード

--- app.rb
+++ app.rb
@@ -3,6 +3,7 @@ require 'sinatra'
 require 'sinatra/reloader'
 require "sinatra/activerecord"
 #require 'sinatra/bootstrap'
+require 'fileutils'
 require 'active_support/all'
 
 require 'sinatra/flash'
@@ -54,10 +55,10 @@ DEFAULT_VIEW_COUNT = 10#300
 
 get '/csv_test' do
   estimates = Estimate.all.order(created_at: :desc)
-  page = (params[:page] || 1).to_i rescue 1
+  @page = (params[:page] || 1).to_i rescue 1
   @view_count = (params[:view_count] || DEFAULT_VIEW_COUNT).to_i rescue DEFAULT_VIEW_COUNT
   Pagy::DEFAULT[:limit] = @view_count
-  @pagy, @list = pagy_array(estimates, page: page)
+  @pagy, @list = pagy_array(estimates, page: @page)
 
   erb :csv_test_index
 end
@@ -73,34 +74,47 @@ post '/csv_test_create' do
 end
 
 get '/download_csv' do
+  redirect to "/csv_test" if params[:filename].blank?
+
+  filename = params[:filename].to_s
+  src = "C:\\Users\\07k11\\Desktop\\work\\sinatratest\\#{filename}"
+  destination = "C:\\Users\\07k11\\Desktop\\整理中\\#{filename}"
+  FileUtils.cp(src, destination)
+
+  flash[:success] = <<-MSG
+      CSVファイル「#{filename}」をダウンロードしました\n
+      #{destination}"
+  MSG
+  redirect to "/csv_test"
 end
 
 
 def proceed_single_number(request_number)
-  Estimate.create!(number: request_number, status: STATUS_ENUM[:wait])
-  flash[:success] = "番号#{request_number}をリクエストしました"
+  Estimate.create!(number: request_number, status: STATUS_ENUM[:wait], csv_filename: "個別入力")
+  flash[:success] = "番号「#{request_number}」をリクエストしました"
 
   # delayedjobに登録
   Delayed::Job.enqueue RecordRequestJob.new(request_numbers: [request_number])
 end
 
 def proceed_multple_numbers(request_number_csv)
-  tempfile = request_number_csv[:tempfile]
-  if File.extname(tempfile.path) != ".csv"
+  file = params[:request_number_csv]
+
+  if file["type"] != "text/csv"
     flash[:fail] = "ファイル形式が異なります。CSVファイルを選択してください。"
     return
   end
 
   request_numbers = []
-  CSV.foreach(tempfile) do |row|
+  CSV.foreach(file["tempfile"]) do |row|
     request_numbers.push row[CSV_AU_COLUMN-1]
   end
 
-  estimates = request_numbers.uniq.map { |request_number| { number: request_number, status: STATUS_ENUM[:wait], created_at: Time.current, updated_at: Time.current } }
+  estimates = request_numbers.uniq.map { |request_number| { number: request_number, status: STATUS_ENUM[:wait], csv_filename: file["filename"], created_at: Time.current, updated_at: Time.current } }
   Estimate.insert_all estimates
 
   # delayedjobに登録
   Delayed::Job.enqueue RecordRequestJob.new(request_numbers: request_numbers)
 
-  flash[:success] = "csvファイル「#{tempfile[:filename]}」記載の番号をリクエストしました"
+  flash[:success] = "csvファイル「#{file["filename"]}」記載の番号をリクエストしました"
 end
--- jobs/record_request_job.rb
+++ jobs/record_request_job.rb
@@ -9,6 +9,8 @@ class RecordRequestJob
     puts @request_numbers.inspect
     return if @request_numbers.blank?
 
+    Estimate.where(number: @request_numbers, status: 0).update_all(status: 1)
+
     output_dir = 'C:/Users/07k11/Desktop/work/sinatratest'
     filename = "#{output_dir}/requests_#{Time.now.in_time_zone('Tokyo').strftime('%Y%m%d%H%M%S')}.csv"
 
@@ -23,5 +25,7 @@ class RecordRequestJob
     # pythonスクリプトの実行
     result = system('python .\test.py')
     puts result
+
+    Estimate.where(number: @request_numbers, status: 1).update_all(status: 2)
   end
 end
